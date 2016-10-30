99f45ea3894da078967fbe0812ef94037bf29608
zt.melody <zt.freedom@gmail.com>
2016-10-30 16:13:05 +0800

    nodejs互斥锁测试

---
+++ tests/thread_test.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import threading
+import requests
+from time import ctime, sleep
+
+
+def get_order(name):
+    for i in range(10):
+        req = requests.get('http://localhost:8000/api/mobilepay/order')
+        if req.text:
+            print '%s(%d):%s' % (name, i, req.text)
+
+
+threads = []
+t1 = threading.Thread(target=get_order, args=('#1',))
+threads.append(t1)
+t2 = threading.Thread(target=get_order, args=('#2',))
+threads.append(t2)
+t3 = threading.Thread(target=get_order, args=('#3',))
+threads.append(t3)
+t4 = threading.Thread(target=get_order, args=('#4',))
+threads.append(t4)
+t5 = threading.Thread(target=get_order, args=('#5',))
+threads.append(t5)
+
+if __name__ == '__main__':
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+
+    for t in threads:
+        t.join()
+    print "all over %s" % ctime()
