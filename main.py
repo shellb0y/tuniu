2907ba93163050070796f3e3d3c15102df0aa4b1
zt.melody <zt.freedom@gmail.com>
2016-10-31 10:09:10 +0800

    adsl测试

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 import service
+import adsl
 
 # trainService = service.TrainOrderService({})
+
+adsl_service = adsl.Adsl()
+adsl_service.set_adsl({"name": u"宽带连接",
+                       "username": "0512...",
+                       "password": "..."})
+
