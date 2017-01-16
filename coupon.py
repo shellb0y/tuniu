68c6f39bbb415d363022a28fbeea378fa97ce163
zhangting <zt.freedom@gmail.com>
2017-01-16 15:46:33 +0800

    modified:   account.txt 	modified:   coupon.py

--- coupon.py
+++ coupon.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 import requests
 
-# import adsl
+import adsl
 
 
 
-# adsl_service = adsl.Adsl({"name": u"宽带连接".encode("gbk"),
-#                        "username": "057474432953",
-#                        "password": "734206"})
+adsl_service = adsl.Adsl({"name": u"宽带连接".encode("gbk"),
+                        "username": "057474432953",
+                        "password": "734206"})
 
-#
-# adsl_service.reconnect()
+
+
 
 f = open("account.txt")  # 返回一个文件对象
 s = open('success.txt', 'a')
+i=0
 for line in open("account.txt"):
+    i=i+1
+    if i%5==0:
+        while 1:
+            try:
+                adsl_service.reconnect()
+                break;
+            except Exception,e:
+                print e.message
+                continue
+    
     tel = line[0:line.find('-')]
     resp = requests.get(
         'http://dynamic.m.tuniu.com/event/lottery/opeLottery'
@@ -22,8 +33,10 @@ for line in open("account.txt"):
     try:
         data = resp.json()
         if data['success']:
-            print tel
-            s.write(tel)
+            print i,tel,'success'
+            s.write(tel+'\n')
+        else:
+            print i,tel,'faild'
     except Exception,e:
         print e.message
 
